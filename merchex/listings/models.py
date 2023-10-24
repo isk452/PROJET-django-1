dd6ad861dbd3a06b1151d3efa3faede1e339000b
ishakdév <144032436+isk452@users.noreply.github.com>
2023-10-24 14:30:57 +0200

    Update models.py
    quand je lance un runserver j'ai un soucis

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
+from datetime import datetime
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(max_length=50)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+         validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
-class Liste(models.Model):
-    name = models.fields.CharField(max_length=100)
